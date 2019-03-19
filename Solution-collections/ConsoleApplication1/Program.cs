284953e4c786d0cc00a77ea07f3bed085aad7d69
Braianxp <braian.xp@gmail.com>
2019-03-19 20:32:45 -0300

    collections mio

--- Solution-collections/ConsoleApplication1/Program.cs
+++ Solution-collections/ConsoleApplication1/Program.cs
@@ -8,8 +8,53 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+
         static void Main(string[] args)
         {
+           List<string> listColors = armarlist(colors);
+           List<string> listRemoveColors = armarlist(removeColors);
+           readList(listColors);
+           List<string> listaremovidos = removeColor(listColors,listRemoveColors);
+           readList(listaremovidos);
         }
+
+        public static List<string> armarlist(string[] S)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                list.Add(S[i]);
+            }
+            return list;
+        }
+
+        public static void readList(List<string> list)
+        {
+            Console.WriteLine("Lista de colores");
+            foreach (var item in list)
+            {
+                Console.WriteLine("Color: {0}", item);
+
+            }
+            Console.ReadKey();
+        }
+
+        public static void removeColor(List<string> listColors,List<string> listRemovecolors)
+        {
+            List<string> listaremovidos = new List<string>();
+
+
+            foreach (var item in listColors)
+            {
+                foreach (var item2 in listRemovecolors)
+                {
+
+                }
+            }
+        }
+
     }
 }
