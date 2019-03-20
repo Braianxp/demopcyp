8a2461ef66e5d96b26fd5f579e41a75339d12b71
Braianxp <braian.xp@gmail.com>
2019-03-19 21:05:41 -0300

    implementado demolista

--- Solution-collections/ConsoleApplication1/Program.cs
+++ Solution-collections/ConsoleApplication1/Program.cs
@@ -13,11 +13,24 @@ namespace ConsoleApplication1
 
         static void Main(string[] args)
         {
-           List<string> listColors = armarlist(colors);
-           List<string> listRemoveColors = armarlist(removeColors);
-           readList(listColors);
-           List<string> listaremovidos = removeColor(listColors,listRemoveColors);
-           readList(listaremovidos);
+            //Demolista();
+            DemoDic();
+        }
+
+        public static void DemoDic()
+        {
+            
+        }
+
+        #region Demolista
+
+        public static void Demolista()
+        {
+            List<string> listColors = armarlist(colors);
+            List<string> listRemoveColors = armarlist(removeColors);
+            readList(listColors);
+            List<string> listaremovidos = removeColor(listColors, listRemoveColors);
+            readList(listaremovidos);
         }
 
         public static List<string> armarlist(string[] S)
@@ -65,6 +78,60 @@ namespace ConsoleApplication1
             }
             return listaremovidos;
         }
+
+        #endregion
+
+        #region Ejemplo de lista
+        private static void DemoLista()
+        {
+            List<string> list = new List<string>();
+
+            // Añade elementos de colors a la Lista (uno por uno)
+            foreach (var color in colors)
+            {
+                list.Add(color);
+            }
+
+            // Añade elementos del arreglo desde el constructor
+            List<string> removeList = new List<string>(removeColors);
+
+            Console.WriteLine("List: ");
+            MostrarLista(list);
+
+            // remueve colores
+            RemueveColores(list, removeList);
+
+            Console.WriteLine("\nList después de llamar a RemueveColores: ");
+            MostrarLista(list);
+        }
+        private static void MostrarLista(List<string> list)
+        {
+            // muestra el contenido con foreach
+            foreach (var element in list)
+            {
+                Console.Write("{0} ", element);
+            }
+
+            // muestra tamaño y capacidad
+            Console.WriteLine("\nTamaño = {0}; Capacidad = {1}",
+               list.Count, list.Capacity);
+
+            int index = list.IndexOf("BLUE");
+
+            if (index != -1)
+                Console.WriteLine("La lista contiene BLUE en índice {0}.",
+                   index);
+            else
+                Console.WriteLine("La lista no contiene BLUE.");
+        }
+        private static void RemueveColores(List<string> firstList, List<string> secondList)
+        {
+            for (int count = 0; count < secondList.Count; ++count)
+            {
+                firstList.Remove(secondList[count]);
+            }
+        }
+        #endregion
 
     }
 }
